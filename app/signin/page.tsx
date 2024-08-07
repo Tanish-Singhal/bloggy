bce76485dc6fb16e8da9878519af78bbed72159f
Tanish Singhal <tanishsinghal510@gmail.com>
2024-08-07 21:50:08 +0530

    signin page with auth logic

---
+++ app/signin/page.tsx
@@ -0,0 +1,126 @@
+// "use server";
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Separator } from "@radix-ui/react-separator";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function Signin() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  
+  const router = useRouter();
+  
+  const handleSignin = async () => {
+    const res = await signIn("credentials", {
+      email: email,
+      password: password,
+      redirect: false,
+    });
+    console.log(res);
+    router.push("/")
+  };
+
+  return (
+    <div className="w-full h-screen lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+      <div className="flex items-center justify-center py-12">
+        <div className="mx-auto grid w-[350px] gap-6">
+          <div className="grid gap-2 text-center">
+            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+              Login
+            </h1>
+
+            <p className="mt-3 text-xl text-muted-foreground">
+              Enter your email below to login to your account
+            </p>
+          </div>
+
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+                <Link
+                  href="/forgot-password"
+                  className="ml-auto inline-block text-sm underline"
+                >
+                  Forgot your password?
+                </Link>
+              </div>
+              <Input
+                id="password"
+                type="password"
+                required
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+
+            <Button type="submit" className="w-full" onClick={handleSignin}>
+              Login
+            </Button>
+
+            <Separator asChild className="bg-background">
+              <div className="py-3 flex items-center text-xs text-muted-foreground uppercase before:flex-[1_1_0%] before:border-t before:border-gray-200 before:me-6 after:flex-[1_1_0%] after:border-t after:border-gray-200 after:ms-6 dark:before:border-gray-700 dark:after:border-gray-700">
+                Or
+              </div>
+            </Separator>
+
+            <Button
+              variant="outline"
+              className="w-full"
+              onClick={async () => {
+                await signIn("google");
+              }}
+            >
+              Login with Google
+            </Button>
+
+            <Button
+              variant="outline"
+              className="w-full"
+              onClick={async () => {
+                await signIn("github");
+              }}
+            >
+              Login with Github
+            </Button>
+          </div>
+
+          <div className="mt-4 text-center text-sm">
+            Don&apos;t have an account?{" "}
+            <Link href="/signup" className="underline">
+              Signup
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <div className="hidden bg-muted lg:block">
+        <Image
+          src="/placeholder.svg"
+          alt="Image"
+          width="1920"
+          height="1080"
+          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
+  );
+}
